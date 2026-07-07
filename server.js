96f0ef4418385e015bab6148140e3060b77aba46
mac <mac@macs-MacBook-Pro.local>
2026-07-07 19:44:08 +0530

    Serve frontend from Express

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 const salesmen = [
   {
     name: "Salesman 1",
