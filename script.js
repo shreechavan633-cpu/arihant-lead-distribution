ef2e86b12acc399c7d1c204c9b529f3c2aa1bb00
mac <mac@macs-MacBook-Pro.local>
2026-07-14 20:25:19 +0530

    Debug response

--- script.js
+++ script.js
@@ -26,5 +26,8 @@ ${String.fromCodePoint(0x1F3E2)} Business Name: ${lead.name}
 ${String.fromCodePoint(0x1F4DE)} Contact Number: ${lead.phone}`;
 
 const url = `https://api.whatsapp.com/send?phone=${data.salesmanPhone}&text=${encodeURIComponent(message)}`;
-window.location.href = url;
+alert(JSON.stringify(data));
+setTimeout(() => {
+    window.location.href = url;
+}, 10000);
 });
